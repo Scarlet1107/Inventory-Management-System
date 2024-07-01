7288b8209c09064cd3adc2fcf6417618ce5c463d
scarlet7 <shougo.ushiro@gmail.com>
2024-07-01 14:28:12 +0900

    fix 404 error

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-      return [
-        {
-          source: '/',
-          destination: '/home',
-          permanent: true, // 永続的なリダイレクト（301リダイレクト）
-        },
-      ];
-    },
-  };
-  
-  export default nextConfig;
-  
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+        permanent: true, // 永続的なリダイレクト（301リダイレクト）
+      },
+    ];
+  },
+  productionBrowserSourceMaps: false,
+};
+
+export default nextConfig;
